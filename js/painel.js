745a751fab69b919c96386083cc03517de3868a8
Victor Moreno <victoormorenoo20@gmail.com>
2026-08-11 11:59:47 -0300

    Criando erro propositalmente

--- js/painel.js
+++ js/painel.js
@@ -7,7 +7,7 @@ async function carregarSensores() {
 }
 
 function converterTemperatura(leitura) {
-  return leitura * 9 / 5 + 32;
+  return leitura * 9 / 5 + 30;
 }
 
 function desenharLinha(sensor) {
